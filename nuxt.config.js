faa6e1348b2f2a56a526bbf3cc22ac5c2bdb34bd
xugenghao <genghao.xu@que360.com>
2021-01-17 20:46:57 +0800

    build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     router: {
         linkActiveClass: 'active'
-
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 3000
     },
     plugins: [
         '~/plugins/request.js',
